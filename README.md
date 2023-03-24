3f0612b40960d876f7336912327168ee97fda78c
威 刘 <lw1140@163.com>
2023-03-24 11:18:59 +0800

    chore: 目录重命名

--- README.md
+++ README.md
@@ -25,3 +25,24 @@ yarn add 软件名 --registry https://registry.npm.taobao.org/
 # win 下 pm2 启动 verdaccio
 pm2 start C:\develop\nvm\npm\node_modules\verdaccio\bin\verdaccio
 ```
+
+`.changeset/conifg.json`
+
+```json:title
+{
+"$schema": "<https://unpkg.com/@changesets/config@2.2.0/schema.json>",
+"changelog": "@changesets/cli/changelog", //更新日志生成函数的加载地址
+"commit": true, //自动提交 version 的改动
+"fixed": [], //捆绑发布的包
+"linked": [],// 配置哪些包要共享版本
+"access": "public", //公开，如果你想阻止一个包被发布到 npm，在包的 package.json 中设置 private: true （可选值：restricted，不公开）
+"baseBranch": "main", //主分支名
+"updateInternalDependencies": "patch", //是否主动更新 package 的依赖，patch、minor
+"ignore": [] //指定不发布的包
+}
+
+```
+
+```
+
+```
--- packages/ui/scripts/build/index.ts
+++ packages/ui/scripts/build/index.ts
@@ -1,4 +1,4 @@
-import { series, parallel, src, dest } from 'gulp';
+import { src, dest } from 'gulp';
 import autoprefixer from 'gulp-autoprefixer';
 import less from 'gulp-less';
 import { resolve } from 'path';
@@ -6,27 +6,27 @@ import delPath from '../utils/delpath';
 import run from '../utils/run';
 
 // 组件库根目录
-export const feUiPackage = resolve(__dirname, '../../');
+export const uiPackage = resolve(__dirname, '../../');
 
 // 删除构建产物
 export const removeDist = () => {
-  console.log(`==============删除目录${feUiPackage}/dist============`);
-  return delPath(`${feUiPackage}/dist`);
+  console.log(`==============删除目录${uiPackage}/dist============`);
+  return delPath(`${uiPackage}/dist`);
 };
 
 // 打包样式
 export const buildStyle = () => {
-  console.log(`==============从${feUiPackage}/components构建样式============`);
-  return src(`${feUiPackage}/components/**/style/**.less`)
+  console.log(`==============从${uiPackage}/components构建样式============`);
+  return src(`${uiPackage}/components/**/style/**.less`)
     .pipe(less())
     .pipe(autoprefixer())
-    .pipe(dest(`${feUiPackage}/dist/lib/components`))
-    .pipe(dest(`${feUiPackage}/dist/es/components`));
+    .pipe(dest(`${uiPackage}/dist/lib/components`))
+    .pipe(dest(`${uiPackage}/dist/es/components`));
 };
 
 // 打包组件
 export const buildComponent = async () => {
-  await run('pnpm run build', feUiPackage);
+  await run('pnpm run build', uiPackage);
 };
 
 async function task() {
